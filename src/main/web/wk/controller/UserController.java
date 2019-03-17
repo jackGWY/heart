97b565ce56317772e8d2b027ce3ee8b44a5fc3d1
jackGWY <guo_wei_yue1@126.com>
2019-03-17 19:10:00 +0800

    后台加入寻找医生模块

---
+++ src/main/web/wk/controller/UserController.java
@@ -0,0 +1,34 @@
+package wk.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import wk.dao.SaveUserInfoMapper;
+import wk.entity.UserPatient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+    @Autowired(required = false)
+    private SaveUserInfoMapper saveUserInfoMapper;
+    @RequestMapping(value = "/getDoctors")
+    public List<UserPatient> getDoctors(){
+        List<UserPatient> users = new ArrayList<>();
+        users=saveUserInfoMapper.getDoctors("doctor");
+        System.out.printf(users.toString());
+        return users;
+    }
+    @RequestMapping(value = "/saveRelation")
+    public JSONObject saveRelation(@RequestParam("patient") String patient, @RequestParam("doctor") String doctor){
+        int i = saveUserInfoMapper.saveRelation(patient,doctor);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("count", i+"");
+        System.out.printf("message:"+i);
+        return jsonObject;
+    }
+}
--- src/main/web/wk/dao/SaveUserInfoMapper.java
+++ src/main/web/wk/dao/SaveUserInfoMapper.java
@@ -1,8 +1,21 @@
 package wk.dao;
 
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
+import wk.entity.UserPatient;
+
+import java.util.List;
 
 public interface SaveUserInfoMapper {
     void saveUser(@Param("phone") String phone, @Param("uname") String uname,@Param("password") String password,@Param("userType") String userType);
     int DoLogin(@Param("uname") String uname,@Param("password") String password);
+
+    @Select("SELECT user_name,user_password,phone,user_type from user_patient where user_type = #{userType}")
+    @Results({@Result(property = "userName", column = "user_name"),
+            @Result(property = "userPassword", column = "user_password"),
+            @Result(property = "userType", column = "user_type")
+    })
+    List<UserPatient> getDoctors(String userType);
+
+    @Insert("REPLACE INTO relation(patient,doctor) values(#{patient},#{doctor})")
+    int saveRelation(@Param("patient")String patient,@Param("doctor")String doctor);
 }
